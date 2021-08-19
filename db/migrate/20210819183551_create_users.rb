ae5e05e8f389755ee8eb787eb6602017dd0f2be2
asb1512 <andrew@olympicmedia.com>
2021-08-19 14:48:26 -0400

    fixed mistake in users table migration

--- db/migrate/20210819183551_create_users.rb
+++ db/migrate/20210819183551_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :email
-      t.username :string
+      t.string :username
       t.password :password_digest
       t.string :bio
       t.string :user_id
