1ab22367739665e20e389356d0eca92347001a02
asb1512 <andrew@olympicmedia.com>
2021-08-19 14:59:19 -0400

    beginning to add data to seed file for national parks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Clear DB each time seed file is run
+# User.delete_all
+# Unsure if all users should be deleted with each seed of the DB
+Park.delete_all
+
+# Creation of National Parks
+national_parks = [
+  {name: 'Acadia', location: 'Maine', image: 'https://upload.wikimedia.org/wikipedia/commons/9/93/Bass_Harbor_Head_Light_Station_2016.jpg'},
+  {name: 'American Samoa'}
+]
+
