d9a88164bab7512422ff25405530f3ed56682410
asb1512 <andrew@olympicmedia.com>
2021-08-21 16:43:44 -0400

    added column 'small_image' to parks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_183726) do
+ActiveRecord::Schema.define(version: 2021_08_21_200844) do
 
   create_table "parks", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_08_19_183726) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "small_image"
   end
 
   create_table "parks_users", id: false, force: :cascade do |t|
