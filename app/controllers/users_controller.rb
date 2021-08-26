c8c2bae1022303a9e5cab156e6980ce4299388a2
asb1512 <andrew@olympicmedia.com>
2021-08-26 18:24:37 -0400

    implemented strong params for security

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
+    user = User.find_by(email: user_params[:email])
     if user
-      render status: 200, message: "User already exists"
+      render status: 409, message: "User already exists"
     else
-      new_user = User.new(email: params[:email], bio: params[:bio])
+      new_user = User.new(email: user_params[:email], bio: user_params[:bio])
       new_user.save
       render json: new_user, status: 200, message: "User successfully created"
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :bio)
   end
 end
