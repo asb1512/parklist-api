16b865dc21e0c8e59161c42921eaf3ed55f958be
asb1512 <andrew@olympicmedia.com>
2021-08-26 11:04:03 -0400

    added create action to handle signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,15 @@ class UsersController < ApplicationController
     users = User.all
     render json: users
   end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user
+      render status: 200, message: "User already exists"
+    else
+      new_user = User.new(email: params[:email], bio: params[:bio])
+      new_user.save
+      render json: new_user, status: 200, message: "User successfully created"
+    end
+  end
 end
