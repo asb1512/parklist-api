097b179d0edde20a90079ad075b71f0e867dc9ba
asb1512 <andrew@olympicmedia.com>
2021-08-27 02:14:41 -0400

    added routes to allow user to add parks to both lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/parks', to: 'parks#index'
-
   post '/users', to: 'users#create'
+  post '/desired-parks', to: 'desired_parks#create'
+  post '/visited-parks', to: 'visited_parks#create'
 end
