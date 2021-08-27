0a6f456f4ad6d2a8d6b14650c3025c55b2774476
asb1512 <andrew@olympicmedia.com>
2021-08-27 17:10:32 -0400

    added admin attribute

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :bio
+  attributes :id, :email, :bio, :admin
   has_many :desired_parks, serializer: DesiredParkSerializer
   has_many :visited_parks, serializer: VisitedParkSerializer
 end
