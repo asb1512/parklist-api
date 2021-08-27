69a92462ab36bb214871994e3432041208abd3a6
asb1512 <andrew@olympicmedia.com>
2021-08-27 17:10:22 -0400

    added route for users#index for admins to access all users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/parks', to: 'parks#index'
+  get '/users', to: 'users#index'
   post '/users', to: 'users#create'
   post '/desired-parks', to: 'desired_parks#create'
   delete '/desired-parks', to: 'desired_parks#destroy'
