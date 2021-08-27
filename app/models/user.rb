d869bc7a87adc9ec3458972f84ce5ac1ee9a32a7
asb1512 <andrew@olympicmedia.com>
2021-08-26 21:46:37 -0400

    removed unnecessary associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ActiveRecord::Base
   # has_secure_password
-  # has_and_belongs_to_many :parks
-  # has_one :visited_park
-  # has_one :desired_park
   has_many :desired_parks
   has_many :visited_parks
   has_many :parks, through: :desired_parks
